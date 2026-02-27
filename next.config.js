944e08c3b981994906626756fa5b1feb6171b5a5
Lucky Loser <lindendnns@yahoo.com>
2026-02-27 16:49:21 +0300

    Erased all files

--- next.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Enable experimental features if needed in the future
-  // For MVP, we keep it simple with default settings
-};
-
-module.exports = nextConfig;
